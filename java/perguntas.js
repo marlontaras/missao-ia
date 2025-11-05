54df0a1b174e3651b1f68cd92901ae382a311737
marlontaras <141860671+marlontaras@users.noreply.github.com>
2025-11-05 14:27:36 -0300

    0511

--- java/perguntas.js
+++ java/perguntas.js
@@ -2,8 +2,94 @@ export const perguntas = [
     {
     enunciado:"Apesar da existência de programas sociais pontuais do governo, esses não têm sido suficientes para romper o ciclo de pobreza e exclusão. O principal problema de sustentabilidade social identificado é a baixa coesão social e a limitada capacidade de autogestão e geração de renda local, resultando em uma dependência crônica de auxílio externo e na estagnação do desenvolvimento humano e comunitário O que vc faria ?"
      alternativas:[
+        texto:"o que envolve ações como o acesso universal à educação de qualidade, à saúde e a oportunidades de emprego digno."
         {
-            texto: "Como uma iniciativa de desenvolvimento sustentável, que priorize a autonomia e a capacitação local, pode ser implementada para fortalecer a coesão social ?"
+            afirmacao:["O acesso a pilares como educação e saúde não são apenas direitos humanos, mas ferramentas cruciais que capacitam os indivíduos, elevam o padrão de vida, reduzem a pobreza e a desigualdade, e fortalecem a capacidade da comunidade de se manter coesa e resiliente perante desafios futuros."
+
+            ]
+            proxima: 1,
         }
+        { 
+             texto: "Como uma iniciativa de desenvolvimento sustentável, que priorize a autonomia e a capacitação local, pode ser implementada para fortalecer a coesão social ?"
+              afirmcao:[
+                "a responsabilidade ambiental (gestão de resíduos e poluição) seja crucial, ela pertence à Sustentabilidade Ambiental. A Sustentabilidade Social foca no ser humano e na estrutura comunitária, abordando questões como justiça social, equidade, acesso a direitos e bem-estar coletivo."
+              ],
+              proxima: 2,
+            }    
      ]
+    },
+ {
+enunciado:"Considerando o conceito de Sustentabilidade Social, seu escopo se estende além das políticas governamentais, alcançando o setor privado por meio da Responsabilidade Social Corporativa (RSC). A RSC exige que as empresas considerem o impacto de suas operações nas pessoas e nas comunidades."
+alternativas:[
+    {
+        texto:
+        afirmacao:[
+
+        ],
+        proxima:3,
+    },
+    {
+       texto:
+       afirmacao:[
+
+       ],
+       proxima:4,
+    }
 ]
+ },
+] {
+enunciado:
+alternativas:[
+    {
+        texto:
+        afirmacao:[
+
+        ],
+        proxima:3,
+    },
+    {
+       texto:
+       afirmacao:[
+
+       ],
+       proxima:4,
+    }
+]
+ },
+  {
+enunciado:
+alternativas:[
+    {
+        texto:
+        afirmacao:[
+
+        ],
+        proxima:3,
+    },
+    {
+       texto:
+       afirmacao:[
+
+       ],
+       proxima:4,
+    
+ },
+  {
+enunciado:
+alternativas:[
+    {
+        texto:
+        afirmacao:[
+
+        ],
+        proxima:3,
+    },
+    {
+       texto:
+       afirmacao:[
+
+       ],
+       proxima:4,
+    }
+]
+ },
